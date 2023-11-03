0cd131c8b868390e5d61872c4be775d9b7a29fb2
gianmaria99 <gianmriav.99@gmail.com>
2023-11-03 15:49:07 +0100

    update system with 2 pi T

--- simulation.py
+++ simulation.py
@@ -11,7 +11,7 @@ def simplify(angle):
         if 0<angle<2*np.pi:
             return angle
 
-def solve_system(noise, zeta_std, phase_std, zetas, phases, l=0.005):
+def solve_system(noise, zeta_std, phase_std, zetas, phases, T=0.57*10**(-9), l=0.005):
     if noise:
         n_zetas = zetas + np.random.normal(0,zeta_std,3)
         n_phases = phases + np.random.normal(0,phase_std,4)
@@ -33,8 +33,8 @@ def solve_system(noise, zeta_std, phase_std, zetas, phases, l=0.005):
     #t[0] = (-2*(beta+epsilon) + np.sqrt((2*(beta+epsilon))**2+4*(alpha+delta+gamma)**2))/(-2*(alpha+delta+gamma))
     #t[1] = (-2*(beta+epsilon) - np.sqrt((2*(beta+epsilon))**2+4*(alpha+delta+gamma)**2))/(-2*(alpha+delta+gamma))
     eta = 2*np.arctan(t)
-    f_d[0] = n_phases[0]-n_phases[1]-(((n_phases[3]-n_phases[1])*(np.cos(n_zetas[0]-eta[0])-np.cos(eta[0])))/(np.cos(n_zetas[2]-eta[0])-np.cos(eta[0])))
-    f_d[1] = n_phases[0]-n_phases[1]-(((n_phases[3]-n_phases[1])*(np.cos(n_zetas[0]-eta[1])-np.cos(eta[1])))/(np.cos(n_zetas[2]-eta[1])-np.cos(eta[1])))
+    f_d[0] = (n_phases[0]-n_phases[1]-(((n_phases[3]-n_phases[1])*(np.cos(n_zetas[0]-eta[0])-np.cos(eta[0])))/(np.cos(n_zetas[2]-eta[0])-np.cos(eta[0]))))/(2*np.pi*T)
+    f_d[1] = (n_phases[0]-n_phases[1]-(((n_phases[3]-n_phases[1])*(np.cos(n_zetas[0]-eta[1])-np.cos(eta[1])))/(np.cos(n_zetas[2]-eta[1])-np.cos(eta[1]))))/(2*np.pi*T)
     v[0] = l*(n_phases[3]-n_phases[1])/(np.cos(n_zetas[2]-eta[0])-np.cos(eta[0]))
     v[1] = l*(n_phases[3]-n_phases[1])/(np.cos(n_zetas[2]-eta[1])-np.cos(eta[1]))
     f_off[0] = n_phases[1]-(v[0]/l*np.cos(eta[0]))
@@ -54,32 +54,42 @@ def check_phases(eta, f_d, v, f_off, zetas, l=0.005):
 def boxplot_plot(errors, xlabel, ylabel, xticks, title, name=''):
     plt.figure(figsize=(12,8))
     plt.ylabel(ylabel)
-    plt.xlabel("zeta standard deviation (°)")
+    plt.xlabel(xlabel)
     plt.grid()
     ax = sns.boxplot(data=errors, orient='v', palette='rocket', showfliers=False)
     plt.xticks(np.arange(len(xticks)), xticks)
     plt.title(title)
     plt.savefig('plots/'+name+'.png')
-    plt.show()
+    #plt.show()
 
-# for i in range(10000):
-#     zetas = np.random.uniform(-np.pi/4,np.pi/4,3)
-#     phases = np.random.uniform(0,2*np.pi,4)
-#     eta_n, f_d_n, v_n, f_off_n, alpha_n, delta_n, gamma_n, n_zetas = solve_system(False, 0, 0, zetas, phases)
-#     c_phases = check_phases(eta_n, f_d_n[0], v_n[0], f_off_n[0], zetas)
-#     for j in range(4):
-#         if c_phases[j]-phases[j] > 10**(-10):
-#             print(c_phases[j]-phases[j])
+zetas = np.random.uniform(-np.pi/4,np.pi/4,2)
+phases = np.random.uniform(0,2*np.pi,5)
+
+
+#for i in range(10000):
+zetas = np.random.uniform(-np.pi/4,np.pi/4,3)
+phases = np.random.uniform(0,2*np.pi,4)
+eta_n, f_d_n, v_n, f_off_n, alpha_n, delta_n, gamma_n, n_zetas = solve_system(False, 0, 0, zetas, phases)
+print(f_d_n)
+print(v_n)
+print(f_off_n)
+
+
+
+# c_phases = check_phases(eta_n, f_d_n[0], v_n[0], f_off_n[0], zetas)
+# for j in range(4):
+#     if c_phases[j]-phases[j] > 10**(-10):
+#         print(c_phases[j]-phases[j])
 
 SNR = np.array([-10,-5,0,5,10,15,20])
 SNR = np.power(10,SNR/10)
 p_std = np.sqrt(1/(2*256*SNR))
 zetas_std = [1,3,5,7,10]
 threshold = 0.05
-for phase_std,snr in zip(p_std,SNR):
+for zeta_std in np.deg2rad(zetas_std):
     tot_eta_errors = []
     tot_fd_errors = []    
-    for zeta_std in np.deg2rad(zetas_std):
+    for phase_std,snr in zip(p_std,SNR):
         N = 10000 # number of simulations
         eta_errors = []
         f_d_errors = []
@@ -117,9 +127,6 @@ for phase_std,snr in zip(p_std,SNR):
             f_off_errors.append(f_off_n-f_off)
         tot_eta_errors.append(eta_errors)
         tot_fd_errors.append(f_d_errors)
-        for e in f_d_errors:
-            if e > 1000:
-                print('e: ' + str(e))
         print('zeta std: ' + str(np.rad2deg(zeta_std)) + '°')
         print('phase_std: ' + str(np.rad2deg(phase_std)) + '°')
         print('SNR: ' + str(10*np.log10(snr)) + ' dB')
@@ -127,5 +134,5 @@ for phase_std,snr in zip(p_std,SNR):
         print('f_d errors mean: ' + str(np.mean(f_d_errors)) + ', std: ' + str(np.std(f_d_errors)))
         print('f_off errors mean: ' + str(np.mean(f_off_errors)) + ', std: ' + str(np.std(f_off_errors)))
         print('\n')
-    boxplot_plot(tot_eta_errors, "zeta standard deviation (°)", "eta errors (°)", zetas_std, "eta errors with SNR= " + str(10*np.log10(snr)) + " dB and threshold= " + str(threshold), 'eta_errors_snr' + str(int(10*np.log10(snr))))
-    boxplot_plot(tot_fd_errors, "zeta standard deviation (°)", "frequency Doppler errors (Hz)", zetas_std, "frequency Doppler errors with SNR= " + str(10*np.log10(snr)) + " dB and threshold= " + str(threshold), 'fd_errors_snr' + str(int(10*np.log10(snr))))
+    boxplot_plot(tot_eta_errors, "SNR (dB)", "eta errors (°)", 10*np.log10(SNR), "eta errors with zeta std = " + str(np.rad2deg(zeta_std)) + "° and threshold= " + str(threshold), 'eta_errors_zeta_std' + str(np.rad2deg(zeta_std)))
+    boxplot_plot(tot_fd_errors, "SNR (dB)", "frequency Doppler errors (Hz)", 10*np.log10(SNR), "frequency Doppler errors with zeta std = " + str(np.rad2deg(zeta_std)) + "° and threshold= " + str(threshold), 'fd_errors_zeta_std' + str(np.rad2deg(zeta_std)))
