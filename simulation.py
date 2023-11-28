4134597e1aa4286e2a318147cbca5f049eab6186
gianmaria99 <gianmriav.99@gmail.com>
2023-11-28 16:30:33 +0100

    averaged results

--- simulation.py
+++ simulation.py
@@ -3,6 +3,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def solve_system(noise, zeta_std, phase_std, zetas, phases, T=0.27*10**(-3), l=0.005):
+    """
+        noise: wether to add noise to phases and angle of arrivals;
+        zeta_std: standard deviation for the noise variable regarding the angle of arrivals;
+        phase_std: standard deviation for the noise variable regarding the measured phase;
+        zetas: angle of arrivals;
+        phases: measured phases;
+        T: sampling period;
+        l: wavelength.
+        Solve the system for the received input parameters and returns the variables of the system.
+    """
     if noise:
         n_zetas = zetas + np.random.normal(0,zeta_std,3)
         n_phases = phases + np.random.normal(0,phase_std,4)
@@ -30,7 +40,10 @@ def solve_system(noise, zeta_std, phase_std, zetas, phases, T=0.27*10**(-3), l=0
     f_off = n_phases[3]/(2*np.pi*T)-((n_phases[2]-n_phases[3])*np.cos(eta[0])/(np.cos(n_zetas[2]-eta[0])-np.cos(eta[0])))
     return eta, f_d, v, f_off, alpha, delta, gamma, n_zetas
 
-def check_phases(eta, f_d, v, f_off, zetas, T=0.27*10**(-3), l=0.005, k=1):
+def get_phases(eta, f_d, v, f_off, zetas, T=0.27*10**(-3), l=0.005, k=1):
+    """
+        Solve the system for the received input parameters and returned the computed phases.
+    """
     c_phases = np.zeros(4)
     c_phases[0] = (2*np.pi*T*(f_d+(v/l*np.cos(zetas[0]-eta))+(k*f_off[0]-(k-1)*f_off[1])))#%np.pi
     c_phases[1] = (2*np.pi*T*(v/l*np.cos(zetas[1]-eta)+(k*f_off[0]-(k-1)*f_off[1])))#%np.pi
@@ -46,7 +59,7 @@ def boxplot_plot(errors, xlabel, ylabel, xticks, title, name=''):
     ax = sns.boxplot(data=errors, orient='v', palette='rocket', showfliers=False)
     plt.xticks(np.arange(len(xticks)), xticks)
     plt.title(title)
-    plt.savefig('plots/'+name+'.png')
+    plt.savefig('plots/averaged_results/'+name+'.png')
     plt.show()
 
 def check_system():
@@ -55,12 +68,18 @@ def check_system():
         phases = np.random.uniform(0,2*np.pi,4)
         eta_n, f_d_n, v_n, f_off_n, alpha_n, delta_n, gamma_n, n_zetas = solve_system(False, 0, 0, zetas, phases)
         
-        c_phases = check_phases(eta_n[0], f_d_n[0], v_n[0], f_off_n[0], zetas)
+        c_phases = get_phases(eta_n[0], f_d_n[0], v_n[0], f_off_n[0], zetas)
         for j in range(4):
             if c_phases[j]-phases[j] > 10**(-10):
                 print(c_phases[j]-phases[j])
 
 def get_input(k=1):
+    """
+    k=1 it represents the discrete time variable, we assume it equal to 1, 
+    but it does not the computation regarding the Doppler frequency.
+    
+    Returns realistic values of: \n measured phases, angle of arrivals, and variables. 
+    """
     f_d = np.random.uniform(-1000,1000)
     f_off = np.random.normal(100000,10000,2)
     v = np.random.uniform(-5,5)
@@ -77,10 +96,12 @@ def get_input(k=1):
                 #print('problem 2')
         if check:
             break
-    phases = check_phases(eta, f_d, v, f_off, zetas, k=k)
+    phases = get_phases(eta, f_d, v, f_off, zetas, k=k)
     return phases, zetas, eta, f_d, v, k*f_off[0]-(k-1)*f_off[1]
 
 SNR = np.array([-10,-5,0,5,10,15,20])
+N = 10000 # number of simulations
+interval = 100 # number of samples in which variables can be considered constant 
 SNR = np.power(10,SNR/10)
 p_std = np.sqrt(1/(2*256*SNR))
 zeta_std = [1,3,5,7,10]
@@ -90,24 +111,32 @@ for z_std in np.deg2rad(zeta_std):
     tot_v_error = []
     tot_f_off_error = []
     for phase_std,snr in zip(p_std,SNR):
-        N = 10000 # number of simulations
         eta_error = []
         f_d_error = []
         f_off_error = []
         v_error = []
         for i in range(N):
             phases,zetas,eta,f_d,v,f_off = get_input()
-            eta_n, f_d_n, v_n, f_off_n, alpha_n, delta_n, gamma_n, n_zetas= solve_system(True, z_std, phase_std, zetas, phases)
-            eta_error.append(eta-eta_n[1])
-            f_d_error.append(f_d-f_d_n[0])
-            v_error.append(v-v_n[1])
-            f_off_error.append(f_off-f_off_n)
+            etas_n = []
+            f_ds_n = []
+            vs_n = []
+            f_offs_n = []
+            for i in range(interval):
+                eta_n, f_d_n, v_n, f_off_n, alpha_n, delta_n, gamma_n, n_zetas= solve_system(True, z_std, phase_std, zetas, phases)
+                etas_n.append(eta_n[1])
+                f_ds_n.append(f_d_n[0])
+                vs_n.append(v_n[1])
+                f_offs_n.append(f_off_n)
+            eta_error.append(eta-np.mean(etas_n))
+            f_d_error.append(np.sqrt((f_d-np.mean(f_ds_n))**2))
+            v_error.append(v-np.mean(vs_n))
+            f_off_error.append(f_off-np.mean(f_offs_n))
         tot_eta_error.append(eta_error)
         tot_f_d_error.append(f_d_error)
         tot_v_error.append(v_error)
         tot_f_off_error.append(f_off_error)
     boxplot_plot(tot_eta_error, "SNR (dB)", "eta errors (°)", 10*np.log10(SNR), "eta errors with zeta std = " + str(np.rad2deg(z_std)) + "°", 'eta_errors_zeta_std' + str(np.round(np.rad2deg(z_std),1)))
-    boxplot_plot(tot_f_d_error[2:], "SNR (dB)", "frequency Doppler errors (Hz)", 10*np.log10(SNR[2:]), "frequency Doppler errors with zeta std = " + str(np.rad2deg(z_std)) + "°", 'bfd_errors_zeta_std' + str(np.round(np.rad2deg(z_std),1)))
+    boxplot_plot(tot_f_d_error, "SNR (dB)", "frequency Doppler errors (Hz)", 10*np.log10(SNR), "frequency Doppler errors with zeta std = " + str(np.rad2deg(z_std)) + "°", 'fd_errors_zeta_std' + str(np.round(np.rad2deg(z_std),1)))
 
 print('eta mean error: ' + str(np.mean(eta_error)))
 print('Doppler mean error: ' + str(np.mean(f_d_error)))
